f525f61826d06ea79c947ee0cc297a0e973f33e3
flenn0324 <zakaria.brahimi@hotmail.com>
2022-07-20 23:12:23 +0100

    Init

---
+++ script.js
@@ -0,0 +1,80 @@
+function changebgcolor()
+{
+    document.getElementById("body").style.backgroundColor="black";
+    document.getElementById("body").style.color="white";
+    document.getElementById("navbg").style.backgroundColor="black";
+    document.getElementById("cont").style.backgroundColor="black";
+    document.getElementById("brand").style.color="white";
+    document.getElementById("navbarNav1").style.color="white";
+    document.getElementById("navbarNav2").style.color="white";
+    document.getElementById("navbarNav3").style.color="white";
+    document.getElementById("navbarNav4").style.color="white";
+    document.getElementById("navbarNav5").style.color="white";
+    document.getElementById("btn1").style.backgroundColor="black";
+    document.getElementById("btn2").style.backgroundColor="black";
+    document.getElementById("btn3").style.backgroundColor="black";
+    document.getElementById("front").style.color="white";
+    document.getElementById("back").style.color="white";
+    document.getElementById("other").style.color="white";
+    document.getElementById("col1").style.backgroundColor="black";
+    document.getElementById("col2").style.backgroundColor="black";
+    document.getElementById("col3").style.backgroundColor="black";
+    document.getElementById("dark").style.display="none";
+    document.getElementById("light").style.display="initial";
+    document.getElementById("li1").style.backgroundColor="black";
+    document.getElementById("li2").style.backgroundColor="black";
+    document.getElementById("li3").style.backgroundColor="black";
+    document.getElementById("li4").style.backgroundColor="black";
+    document.getElementById("li5").style.backgroundColor="black";
+    document.getElementById("li6").style.backgroundColor="black";
+    document.getElementById("li7").style.backgroundColor="black";
+    document.getElementById("footer").style.backgroundColor="black";
+    document.getElementById("footer").style.color="white";
+    document.getElementById("prj1").style.color="white";
+    document.getElementById("prj2").style.color="white";
+    document.getElementById("prj3").style.color="white";
+    document.getElementById("prj1").style.backgroundColor="black";
+    document.getElementById("prj2").style.backgroundColor="black";
+    document.getElementById("prj3").style.backgroundColor="black";
+}
+
+function lightmode()
+{
+    document.getElementById("body").style.backgroundColor="white";
+    document.getElementById("body").style.color="black";
+    document.getElementById("navbg").style.backgroundColor="white";
+    document.getElementById("cont").style.backgroundColor="white";
+    document.getElementById("brand").style.color="black";
+    document.getElementById("navbarNav1").style.color="rgb(121, 7, 131)";
+    document.getElementById("navbarNav2").style.color="rgb(121, 7, 131)";
+    document.getElementById("navbarNav3").style.color="rgb(121, 7, 131)";
+    document.getElementById("navbarNav4").style.color="rgb(121, 7, 131)";
+    document.getElementById("navbarNav5").style.color="rgb(121, 7, 131)";
+    document.getElementById("btn1").style.backgroundColor="white";
+    document.getElementById("btn2").style.backgroundColor="white";
+    document.getElementById("btn3").style.backgroundColor="white";
+    document.getElementById("front").style.color="black";
+    document.getElementById("back").style.color="black";
+    document.getElementById("other").style.color="black";
+    document.getElementById("col1").style.backgroundColor="white";
+    document.getElementById("col2").style.backgroundColor="white";
+    document.getElementById("col3").style.backgroundColor="white";
+    document.getElementById("dark").style.display="initial";
+    document.getElementById("light").style.display="none";
+    document.getElementById("li1").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("li2").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("li3").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("li4").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("li5").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("li6").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("li7").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("footer").style.backgroundColor="rgb(121, 7, 131)";
+    document.getElementById("footer").style.color="white";
+    document.getElementById("prj1").style.color="black";
+    document.getElementById("prj2").style.color="black";
+    document.getElementById("prj3").style.color="black";
+    document.getElementById("prj1").style.backgroundColor="white";
+    document.getElementById("prj2").style.backgroundColor="white";
+    document.getElementById("prj3").style.backgroundColor="white";
+
+}
